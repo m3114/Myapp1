10bc14219ed829299082332a2defd3a975a9b85c
김다은 <softeun92@naver.com>
2021-11-13 10:57:13 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/myapp1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.nepplus.myapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener {
+            Log.d("메인화면","로그 찍기버튼 누름")
+            Log.e("메인화면", "e로 로그 찍기 버튼 누름")
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
